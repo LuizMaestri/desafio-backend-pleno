a8f962b8c1ea99c4f3f1407f798218cdedbe25a2
Luiz Maestri <luiz.maestri@neoway.com.br>
2019-09-16 11:23:02 -0300

    fix script to run in linux

--- execute.sh
+++ execute.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn clean install && docker-compose up --build -d && docker-compose logs --no-color -f > log.txt &
+mvn clean install && sudo docker-compose up --build -d && sudo docker-compose logs --no-color -f > log.txt &
